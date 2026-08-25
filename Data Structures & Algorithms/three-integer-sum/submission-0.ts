3f55eb56b0881fc831a3d6e46a37f6a535fed109
Ali Khalill <61080704+alikhalilll@users.noreply.github.com>
2026-08-25 21:56:45 +0300

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.ts
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        /**
+         * every array should not have the all the same elements
+         * the sum of all of the element in all array are equal 0
+         */
+        const result: number[][] = []
+        nums.sort((a,b)=> a - b)
+        const n = nums.length 
+
+        console.log({
+            sortedNums: nums
+        })
+
+        for(let i = 0; i < n; i++){
+            if (i > 0 && nums[i] === nums[i - 1]) continue 
+
+            let left = i + 1
+            let right = n - 1
+            while(left< right){
+                const sum = nums[i] + nums[left] + nums[right]
+                if(sum > 0) right--
+                else if (sum < 0) left++
+                else{
+                    result.push([nums[i],nums[left],nums[right]])
+                    left++
+                    right--
+                    while(left < right && nums[left] === nums[left - 1]) left++
+                    // while(left < right && nums[right] === nums[right + 1]) right--
+                }
+            }
+        }
+        return result
+    }
+}
