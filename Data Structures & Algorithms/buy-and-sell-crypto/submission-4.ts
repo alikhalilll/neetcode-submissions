69889d53302ef9fdba70823534ad33591cb33f17
Ali Khalill <61080704+alikhalilll@users.noreply.github.com>
2026-09-02 21:23:38 +0300

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        // let minPrice = prices[0]
+        // let bestPrice = 0
+        // for(const price of prices){
+        //     bestPrice = Math.max(bestPrice, price - minPrice)
+        //     minPrice = Math.min(minPrice, price)
+        // }
+
+        let l = 0
+        let r = l + 1
+        let best = 0
+        while(r < prices.length){
+            if(prices[l] < prices[r]){
+                best = Math.max(best, prices[r] - prices[l])
+                r++
+            }
+            else {l = r;r = l + 1}
+        
+        }
+        return best
+    }
+}
