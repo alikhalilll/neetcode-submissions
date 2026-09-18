d9c7dc61f4316ce1da4ab0092e7c855ccd2232ff
Ali Khalill <61080704+alikhalilll@users.noreply.github.com>
2026-09-18 21:42:05 +0300

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.ts
@@ -0,0 +1,25 @@
+class MinStack {
+  private stack: number[] = [];
+  private minStack: number[] = [];
+
+  push(val: number): void {
+    this.stack.push(val);
+    const currentMin = this.minStack.length
+      ? this.minStack[this.minStack.length - 1]
+      : val;
+    this.minStack.push(Math.min(val, currentMin));
+  }
+
+  pop(): void {
+    this.stack.pop();
+    this.minStack.pop();
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
